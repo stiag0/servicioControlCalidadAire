9102a49f63c22466b3a4dffb78d23965d6e77666
stiag0 <stiago01r@gmail.com>
2019-09-21 15:10:15 -0500

    cambio de formato hora a segundos, para facilitar comparacion

--- main/model/model.py
+++ main/model/model.py
@@ -13,22 +13,27 @@ def connection(collection):
 def db_save(collection, document):
     try:
         db_connection = connection(collection)
-        
         #if db_connection.find({_codigo: document["codigo"]}) == {}:
         find_response = db_connection.find_one({'codigo': document["codigo"]})
 
         if find_response != None:
             """print(find_response['mediciones'][len(find_response['mediciones'])-1]["fecha_hora"],document["fecha_hora"])"""
-            str1 = str(find_response['mediciones'][len(find_response['mediciones'])-1]["fecha_hora"]) 
-            str2 = str(document["fecha_hora"])
+            str1 = find_response['mediciones'][len(find_response['mediciones'])-1]["fecha_segundos"] 
+            str2 = document["fecha_hora"]
 
-            #a = datetime.datetime.strptime(str1, '%y-%m-%d %H:%M:%S').time()
-            #b = datetime.datetime.strptime(str2, '%y-%m-%d %H:%M:%S').time()
-            #print(a,b)
+            a = int(str2[:4])
+            m = int(str2[5:7])
+            d = int(str2[8:10])
+            h = int(str2[11:13])
+            mi = int(str2[14:16])
+            s = int(str2[17:19])
+            fS = (a*31536000)+(m*2592000)+(d*86400)+(h*3600)+(mi*60)+(s)
             #ideal a < b
-            if  str1 != str2:
+            print(str1,"<",fS)
+            if  str1 < fS:
+                print("si")
                 medicion ={
-                "fecha_hora": document["fecha_hora"],
+                "fecha_segundos": fS,
                 "PM2_5_CC_ICA":document["PM2_5_CC_ICA"],
                 "PM2_5_mean":document["PM2_5_mean"],
                 "PM2_5_last": document["PM2_5_last"],
@@ -41,11 +46,20 @@ def db_save(collection, document):
                         {'$push': {'mediciones': medicion}}
                     )
                 except:
+                    print("NO pudo actualizar")
                     return False
-
+            else:
+                print("NO")
         else:
             print("> Se detectó un nuevo sensor!")
             print("> Id: ", document["nombre"])
+            a = int(document["fecha_hora"][:4])
+            m = int(document["fecha_hora"][5:7])
+            d = int(document["fecha_hora"][8:10])
+            h = int(document["fecha_hora"][11:13])
+            mi = int(document["fecha_hora"][14:16])
+            s = int(document["fecha_hora"][17:19])
+            fS = (a*31536000)+(m*2592000)+(d*86400)+(h*3600)+(mi*60)+(s)
             newSensor = {
             "altitud": document["altitud"],
             "barrio": document["barrio"],
@@ -57,7 +71,7 @@ def db_save(collection, document):
             "longitude": document["longitude"],
             "mediciones":[
                 {
-                "fecha_hora": document["fecha_hora"],
+                "fecha_segundos": fS,
                 "PM2_5_CC_ICA":document["PM2_5_CC_ICA"],
                 "PM2_5_mean":document["PM2_5_mean"],
                 "PM2_5_last": document["PM2_5_last"],
