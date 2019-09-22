7cf46efe2fcad73cd84cc4d4b3f8e12b18b6bf1f
stiag0 <stiago01r@gmail.com>
2019-09-21 19:22:37 -0500

    implementacion de almacenamiento en cualquier indice de mediciones

--- main/model/model.py
+++ main/model/model.py
@@ -17,7 +17,20 @@ def transformTS (sdateFormat):
     s = int(sdateFormat[17:19])
     fS = (a*31536000)+(m*2592000)+(d*86400)+(h*3600)+(mi*60)+(s)
     return fS
-# This method recives the collection(string) where we want to save it in and the dictionary to save.
+def searchOL(lista,fs):
+    i = len(lista)-1
+    for medicion in reversed(lista):
+        if medicion["fecha_segundos"]==fs:
+            print("falsa alrma")
+            return False
+        if medicion["fecha_segundos"]<fs:
+            print(i,"es menor")
+            return i+1
+        else:
+            i = i-1
+            
+# This method recives the collection(s
+# tring) where we want to save it in and the dictionary to save.
 def db_save(collection, document):
     try:
         db_connection = connection(collection)
@@ -28,10 +41,9 @@ def db_save(collection, document):
             """print(find_response['mediciones'][len(find_response['mediciones'])-1]["fecha_hora"],document["fecha_hora"])"""
             str1 = find_response['mediciones'][len(find_response['mediciones'])-1]["fecha_segundos"] 
             str2 = transformTS(document["fecha_hora"]) 
-            #ideal a < b
-            print(str1,"<",str2)
-            if  str1 < str2:
-                print("si")
+            a =searchOL(find_response['mediciones'],str2)
+            
+            if  a == len(find_response['mediciones']):
                 medicion ={
                 "fecha_segundos": str2,
                 "PM2_5_CC_ICA":document["PM2_5_CC_ICA"],
@@ -40,16 +52,41 @@ def db_save(collection, document):
                 "temperatura": document["temperatura"],
                 "humedad_relativa": document["humedad_relativa"]
                 }
+                print("si")
                 try:
                     update_response = db_connection.update(
                         {"codigo": document["codigo"]},
                         {'$push': {'mediciones': medicion}}
                     )
+                    
                 except:
                     print("NO pudo actualizar")
                     return False
             else:
-                print("NO")
+                if a < len(find_response['mediciones']):
+                    medicion ={
+                    "fecha_segundos": str2,
+                    "PM2_5_CC_ICA":document["PM2_5_CC_ICA"],
+                    "PM2_5_mean":document["PM2_5_mean"],
+                    "PM2_5_last": document["PM2_5_last"],
+                    "temperatura": document["temperatura"],
+                    "humedad_relativa": document["humedad_relativa"]
+                    }
+                    print("si interno")
+                    try:
+                        update_response = db_connection.update(
+                            {"codigo": document["codigo"]},
+                            {'$push': {"mediciones": {
+                                        "$each": [medicion],
+                                        "$position": a
+                                        }
+                                    }
+                            }
+                        )
+                        
+                    except:
+                        print("NO pudo actualizar")
+                        return False
         else:
             print("> Se detectó un nuevo sensor!")
             print("> Id: ", document["nombre"])
@@ -87,3 +124,5 @@ def db_save(collection, document):
         return True
     except:
         return False
+#def searchBetween(date1,date2):
+    
